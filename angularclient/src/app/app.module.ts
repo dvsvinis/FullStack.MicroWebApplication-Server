7cdb43b7dfb5caabac73dbd1f7f040670436f1d0
donnasvinis <dvsvinis@mac.com>
2019-07-14 17:46:53 -0400

    Video forms working

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -7,12 +7,17 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
 import { UserService } from './services/user-service.service';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { VideoFormComponent } from './components/video-form/video-form.component';
+import { VideoService } from './services/video-service.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
-    UserFormComponent
+    UserFormComponent,
+    VideoListComponent,
+    VideoFormComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +25,7 @@ import { UserService } from './services/user-service.service';
     HttpClientModule,
     FormsModule
   ],
-  providers: [UserService],
+  providers: [UserService, VideoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
