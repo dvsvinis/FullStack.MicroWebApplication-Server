778ba0aa0859e15340a8e3c18741a2f875aded1d
donnasvinis <dvsvinis@mac.com>
2019-07-18 07:12:33 -0400

    addUser working and addvideo working

--- src/main/resources/static/angularclient/src/app/app-routing.module.ts
+++ src/main/resources/static/angularclient/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
-// import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 import { VideoListComponent } from './components/video-list/video-list.component';
 import { VideoFormComponent } from './components/video-form/video-form.component';
 import { commentListComponent } from './components/comment-list/comment-list.component';
@@ -14,7 +14,7 @@ import { AddUserComponent } from './components//add-user/add-user.component';
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
-  // { path: 'adduser', component: UserFormComponent },
+  { path: 'adduser', component: UserFormComponent },
   { path: 'videos', component: VideoListComponent },
   { path: 'addvideo', component: VideoFormComponent },
   { path: 'comments', component: commentListComponent },
