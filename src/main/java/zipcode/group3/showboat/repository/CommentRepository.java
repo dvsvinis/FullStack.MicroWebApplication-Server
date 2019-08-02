afb9d0b2db3379ef2679d4e67d1eb6b78e9be12c
AngelicaSantos <angelica.santos2113@gmail.com>
2019-08-02 12:07:27 -0400

    connected video to comment

--- src/main/java/zipcode/group3/showboat/repository/CommentRepository.java
+++ src/main/java/zipcode/group3/showboat/repository/CommentRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByVideoid(Long videoid);
+    List<Comment> findByVideoId(Long videoid);
 
 }
